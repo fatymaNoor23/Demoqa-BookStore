08577ecdba7831451ff0f7a8d760ab070d5317b8
fatymaNoor23 <fatima.noor@emumba.com>
2023-05-30 16:28:22 +0500

    completely removed unusend and commented import in the step file

--- tests/step-definitions/BookStore.step.ts
+++ tests/step-definitions/BookStore.step.ts
@@ -1,15 +1,12 @@
+const { Given, When, Then } = require('@wdio/cucumber-framework');
 import { clickBackToStoreButton } from "../../main/bookPageDetails/tasks/bookPageDetails.tasks";
 import { verifyBacktoStoreUrl, verifySubTitleName, verifyTitleName, verifyWebUrl, verifypageNumbers } from "../../main/bookPageDetails/assertions/bookPageDetail.assertions";
-//import { clickingBackButton } from "../../main/bookPageDetails/tasks/bookPageDetails.tasks";
-import { verifyAuthorOfBook, verifyAuthorTabDisplay, verifyCorrectUrl, verifyImageTabDisplay, verifyImgUrlOfBook, verifyLoginBtnDisplay, verifyNameOfBook, verifyPublisherOfBook, verifyPublisherTabDisplay, verifySearchBtnDisplay, verifyTitleTabDisplay } from "../../main/bookStore/assertions/bookStore.assertions";
+import * as info from "../../main/bookStore/assertions/bookStore.assertions";
 import { clickBook, searchBook } from "../../main/bookStore/tasks/bookStore.tasks";
 import { clickBooksWidget } from "../../main/demoQaPage/actions/demoQa.actions";
 
-const { Given, When, Then } = require('@wdio/cucumber-framework');
-
 Given("the user is on the DemoQA website",async()=>{
     await browser.url("https://demoqa.com/");
-
 })
 
 When("the user click on the Books widget", async()=>{
@@ -21,12 +18,12 @@ Then("the user should be navigated to the Books page",async()=>{
 })
 
 Then("the user should see the search bar, login button, image, title, author, and publisher tab",async()=>{
-    await verifySearchBtnDisplay();
-    await verifyLoginBtnDisplay();
-    await verifyImageTabDisplay();
-    await verifyTitleTabDisplay();
-    await verifyAuthorTabDisplay();
-    await verifyPublisherTabDisplay();
+    await info.verifySearchBtnDisplay();
+    await info.verifyLoginBtnDisplay();
+    await info.verifyImageTabDisplay();
+    await info.verifyTitleTabDisplay();
+    await info.verifyAuthorTabDisplay();
+    await info.verifyPublisherTabDisplay();
 })
 
 //Scenario
@@ -35,17 +32,17 @@ When("the user type 'You Don't Know JS' in the search bar and search", async()=>
 })
 
 Then("the user should see the title, author name, publisher name, and image URL",async()=>{
-    await verifyNameOfBook();
-    await verifyAuthorOfBook();
-    await verifyPublisherOfBook();
-    await verifyImgUrlOfBook();
+    await info.verifyNameOfBook();
+    await info.verifyAuthorOfBook();
+    await info.verifyPublisherOfBook();
+    await info.verifyImgUrlOfBook();
 })
 When("the user click on the 'You Don't Know JS' option", async()=>{
     await clickBook();
 })
 
 Then("the user should be navigated to the correct URL",async()=>{
-    await verifyCorrectUrl();
+    await info.verifyCorrectUrl();
 })
 
 Then("the user should see the title, subtitles, page numbers,description and website URL",async()=>{
