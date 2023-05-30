01b75291bb94b43f6570a9403307013f91f313d0
fatymaNoor23 <fatima.noor@emumba.com>
2023-05-30 14:37:24 +0500

    added assertion for discription of book for bookPageDetail

--- main/bookPageDetails/assertions/bookPageDetail.assertions.ts
+++ main/bookPageDetails/assertions/bookPageDetail.assertions.ts
@@ -1,26 +1,33 @@
 import assert from 'soft-assert'
+import data from '../../utils/bookInfor.json';
 import { pages, subTitle, titleName, webUrl } from '../user_interface/bookPageDetail.ui';
 export async function verifyTitleName() {
-    const titleText=await titleName.getText();
+    const titleText=await data.Title;
     await assert.softAssert( titleText,"You Don't Know JS", "error",[]);
     await assert.softAssertAll();
 }
 
 export async function verifySubTitleName() {
-    const subTitleText=await subTitle.getText();
+    const subTitleText=await data.subTitle;
     await assert.softAssert( subTitleText ,"ES6 & Beyond", "error",[]);
     await assert.softAssertAll();
 }
 
 export async function verifypageNumbers() {
-    const pageText=await pages.getText();
-    await assert.softAssert(pageText,"278","error",[]);
+    const pageText=data.Pages;
+    await assert.softAssert(pageText,278,"error",[]);
     await assert.softAssertAll();
 }
 
 export async function verifyWebUrl() {
-    const urlText=(await webUrl).getText();
+    const urlText=await data.Website;
     await assert.softAssert(await urlText,"https://github.com/getify/You-Dont-Know-JS/tree/master/es6%20&%20beyond","error",[]);
+    await assert.softAssertAll();
+}
+
+export async function verifyDescriptionOfBook(){
+    const discrption=await data.Description;
+    await assert.softAssert(discrption,"No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. As part of the \"You Don’t Know JS\" series, this compact guide focuses on new features available in ECMAScript 6 (ES6), the latest version of the st","error",[])
     await assert.softAssertAll();
 }
 
