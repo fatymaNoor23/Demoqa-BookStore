aa60374df993ed45c32e152543823fe7e2dcb963
fatymaNoor23 <fatima.noor@emumba.com>
2023-05-30 12:41:46 +0500

    Book Page detail user-interface

---
+++ main/bookPageDetails/user_interface/bookPageDetail.ui.ts
@@ -0,0 +1,9 @@
+export const titleName=$(`//div//label[contains(@id,"userName-value") and contains(.,"You Don't Know JS")]`);
+export const subTitle=$(`//div//label[contains(@id,"userName-value") and contains(.,"ES6 & Beyond")]`);
+export const pages=$(`//div//label[contains(@id,"userName-value") and contains(.,"278")]`);
+export const  webUrl=$(`//div//label[contains(@id,"userName-value") and contains(.,"https://github.com/getify/You-Dont-Know-JS/tree/master/es6%20&%20beyond")]`);
+export const backButton=$(`//div//button[contains(@class,"btn btn-primary") and contains(.,"Back To Book Store")]`)
+
+
+
+
