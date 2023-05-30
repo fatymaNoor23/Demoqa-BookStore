15dcd972e872415a89b2159b39ee839f884a3e75
fatymaNoor23 <fatima.noor@emumba.com>
2023-05-30 12:45:35 +0500

    selector for the page showing multiple book items

---
+++ main/bookStore/user_interface/bookStore.ui.ts
@@ -0,0 +1,8 @@
+export const searchBox=$('//div//input[contains(@id,"searchBox")]');
+export const loginButton=$('//div//button[contains(@id,"login")]')
+export const imageTab=$('//div[contains(@class,"rt-resizable-header-content") and contains(.,"Image")]')
+export const titleTab=$('//div[contains(@class,"rt-resizable-header-content") and contains(.,"Title")]')
+export const authorTab=$('//div[contains(@class,"rt-resizable-header-content") and contains(.,"Author")]')
+export const publisherTab=$('//div[contains(@class,"rt-resizable-header-content") and contains(.,"Publisher")]')
+export const searchIcon=$('//div//input[contains(@id,"searchBox")]/following::span[contains(@id,"basic-addon2")]')
+export const bookName=$(`//div//a[contains(.,"You Don't Know JS")]`);
