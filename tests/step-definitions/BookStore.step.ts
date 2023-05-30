d53d7fec4dd15d79c5c5a1eabbdc87009067eeec
fatymaNoor23 <fatima.noor@emumba.com>
2023-05-30 16:43:20 +0500

    merged multiple imports into single BookDetail using *

--- tests/step-definitions/BookStore.step.ts
+++ tests/step-definitions/BookStore.step.ts
@@ -1,9 +1,9 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 import { clickBackToStoreButton } from "../../main/bookPageDetails/tasks/bookPageDetails.tasks";
-import { verifyBacktoStoreUrl, verifySubTitleName, verifyTitleName, verifyWebUrl, verifypageNumbers } from "../../main/bookPageDetails/assertions/bookPageDetail.assertions";
+import * as bookDetail from "../../main/bookPageDetails/assertions/bookPageDetail.assertions";
 import * as info from "../../main/bookStore/assertions/bookStore.assertions";
 import { clickBook, searchBook } from "../../main/bookStore/tasks/bookStore.tasks";
-import { clickBooksWidget } from "../../main/demoQaPage/actions/demoQa.actions";
+import {  clickBooksWidget  } from "../../main/demoQaPage/actions/demoQa.actions";
 
 Given("the user is on the DemoQA website",async()=>{
     await browser.url("https://demoqa.com/");
@@ -46,10 +46,10 @@ Then("the user should be navigated to the correct URL",async()=>{
 })
 
 Then("the user should see the title, subtitles, page numbers,description and website URL",async()=>{
-    await verifyTitleName();
-    await verifySubTitleName();
-    await verifypageNumbers();
-    await verifyWebUrl();
+    await bookDetail.verifyTitleName();
+    await bookDetail.verifySubTitleName();
+    await bookDetail.verifypageNumbers();
+    await bookDetail.verifyWebUrl();
 })
 
 When("the user click on the 'Back to Book Store' page", async()=>{
@@ -57,5 +57,5 @@ When("the user click on the 'Back to Book Store' page", async()=>{
 })
 
 Then("the user should be navigated back to the Books page",async()=>{
-    await verifyBacktoStoreUrl();
+    await bookDetail.verifyBacktoStoreUrl();
 })
